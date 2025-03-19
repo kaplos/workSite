f175a3608e013cfaa80bf07716297ff10666b59e
= <=>
2025-03-19 15:26:16 -0400

    trying some changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base:'./',
+  css: {
+    postcss: './postcss.config.js', // Ensure PostCSS is configured
+  },
   plugins: [react()],
 })
